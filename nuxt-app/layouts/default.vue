630af3f0ef89fc14f44914759f75885deae80ea5
Grzegorz Bujański <grzegorz.bujanski@gmail.com>
2025-01-14 08:41:51 +0100

    feat: add basic layout

---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <p>NNP</p>
+    </div>
+    <div class="channel-container">
+        <div class="channel-list">
+            <span class="channel-name channel-name-active">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name">sidebar</span>
+            <span class="channel-name channel-name-add">new channel</span>
+        </div>
+        <div class="channel-content">
+            <slot />
+        </div>
+    </div>
+</template>
